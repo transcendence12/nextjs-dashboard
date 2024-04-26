27d60e6068ed7f5ae9203e65600638c3718e1938
Gosia Krawczuk <gosiek.krawczuk@gmail.com>
2024-04-26 15:44:11 +0200

    added metadata to the app

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Customers Dashboard',
+}
+
 export default function Page(){
     return <p>Customers Page!</p>
 }
